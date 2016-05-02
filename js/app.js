c30d695f97a06d9ca1fb95032a25dec975f52c19
Anthony Mejia <anthony.mejia1194@gmail.com>
2016-05-02 08:04:19 -0400

    Updated main controller

---
+++ js/app.js
@@ -0,0 +1,58 @@
+(function () {
+  'use strict';
+  angular
+      .module('autocompleteCustomTemplateDemo', ['ngMaterial'])
+      .controller('DemoCtrl', DemoCtrl);
+  function DemoCtrl ($timeout, $q, $log) {
+    var self = this;
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+    self.repos         = loadAll();
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for repos... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.repos.filter( createFilterFor(query) ) : self.repos,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
+    }
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    }
+    /**
+     * Build `components` list of key/value pairs
+     */
+    function loadAll() {
+      var repos = cities;
+      return repos.map( function (repo) {
+        repo.value = repo.city.toLowerCase();
+        return repo;
+      });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(item) {
+        return (item.value.indexOf(lowercaseQuery) === 0);
+      };
+    }
+  }
+})();
---
+++ js/cities.js
@@ -0,0 +1,1002 @@
+var cities = [
+ {
+   "city":"New York",
+   "state":" NY",
+   "id": "5128581"
+ },
+ {
+   "city":"Los Angeles",
+   "state":" CA",
+   "id": "5368361"
+ },
+ {
+   "city":"Chicago",
+   "state":" IL",
+   "id": "4887398"
+ },
+ {
+   "city":"Houston",
+   "state":" TX",
+   "id": "4699066"
+ },
+ {
+   "city":"Philadelphia",
+   "state":" PA",
+   "id": "4560349"
+ },
+ {
+   "city":"Phoenix",
+   "state":" AZ",
+   "id": "5308655"
+ },
+ {
+   "city":"San Antonio",
+   "state":" TX",
+   "id": "4726206"
+ },
+ {
+   "city":"San Diego",
+   "state":" CA",
+   "id": "5391811"
+ },
+ {
+   "city":"Dallas",
+   "state":" TX",
+   "id": "4684888"
+ },
+ {
+   "city":"San Jose",
+   "state":" CA",
+   "id": "5392171"
+ },
+ {
+   "city":"Austin",
+   "state":" TX",
+   "id": "4671654"
+ },
+ {
+   "city":"Jacksonville",
+   "state":" FL",
+   "id": "4160021"
+ },
+ {
+   "city":"Indianapolis",
+   "state":" IN",
+   "id": "4259418"
+ },
+ {
+   "city":"San Francisco",
+   "state":" CA",
+   "id": "5391959"
+ },
+ {
+   "city":"Columbus",
+   "state":" OH",
+   "id": "4509177"
+ },
+ {
+   "city":"Fort Worth",
+   "state":" TX",
+   "id": "4691930"
+ },
+ {
+   "city":"Charlotte",
+   "state":" NC",
+   "id": "4460243"
+ },
+ {
+   "city":"Detroit",
+   "state":" MI",
+   "id": "4990729"
+ },
+ {
+   "city":"El Paso",
+   "state":" TX",
+   "id": "5520993"
+ },
+ {
+   "city":"Memphis",
+   "state":" TN",
+   "id": "4641239"
+ },
+ {
+   "city":"Boston",
+   "state":" MA",
+   "id": "4930956"
+ },
+ {
+   "city":"Seattle",
+   "state":" WA",
+   "id": "5809844"
+ },
+ {
+   "city":"Denver",
+   "state":" CO",
+   "id": "5419384"
+ },
+ {
+   "city":"Washington",
+   "state":" DC",
+   "id": "4372777"
+ },
+ {
+   "city":"Nashville",
+   "state":" TN",
+   "id": "4644585"
+ },
+ {
+   "city":"Baltimore",
+   "state":" MD",
+   "id": "4347820"
+ },
+ {
+   "city":"Louisville",
+   "state":" KY",
+   "id": "4299276"
+ },
+ {
+   "city":"Portland",
+   "state":" OR",
+   "id": "5746545"
+ },
+ {
+   "city":"Oklahoma City",
+   "state":" OK",
+   "id": "4544379"
+ },
+ {
+   "city":"Milwaukee",
+   "state":" WI",
+   "id": "5263058"
+ },
+ {
+   "city":"Las Vegas",
+   "state":" NV",
+   "id": "5506956"
+ },
+ {
+   "city":"Albuquerque",
+   "state":" NM",
+   "id": "5454711"
+ },
+ {
+   "city":"Tucson",
+   "state":" AZ",
+   "id": "5318313"
+ },
+ {
+   "city":"Fresno",
+   "state":" CA",
+   "id": "5350937"
+ },
+ {
+   "city":"Sacramento",
+   "state":" CA",
+   "id": "5389489"
+ },
+ {
+   "city":"Long Beach",
+   "state":" CA",
+   "id": "5367929"
+ },
+ {
+   "city":"Kansas City",
+   "state":" MO",
+   "id": "4273837"
+ },
+ {
+   "city":"Mesa",
+   "state":" AZ",
+   "id": "5304391"
+ },
+ {
+   "city":"Virginia Beach",
+   "state":" VA",
+   "id": "4791259"
+ },
+ {
+   "city":"Atlanta",
+   "state":" GA",
+   "id": "4180439"
+ },
+ {
+   "city":"Colorado Springs",
+   "state":" CO",
+   "id": "5417598"
+ },
+ {
+   "city":"Raleigh",
+   "state":" NC",
+   "id": "4487042"
+ },
+ {
+   "city":"Omaha",
+   "state":" NE",
+   "id": "5074472"
+ },
+ {
+   "city":"Miami",
+   "state":" FL",
+   "id": "4164238"
+ },
+ {
+   "city":"Oakland",
+   "state":" CA",
+   "id": "5378538"
+ },
+ {
+   "city":"Tulsa",
+   "state":" OK",
+   "id": "4553433"
+ },
+ {
+   "city":"Minneapolis",
+   "state":" MN",
+   "id": "5037649"
+ },
+ {
+   "city":"Cleveland",
+   "state":" OH",
+   "id": "5150529"
+ },
+ {
+   "city":"Wichita",
+   "state":" KS",
+   "id": "4281730"
+ },
+ {
+   "city":"Arlington",
+   "state":" TX",
+   "id": "4671240"
+ },
+ {
+   "city":"New Orleans",
+   "state":" LA",
+   "id": "4335045"
+ },
+ {
+   "city":"Bakersfield",
+   "state":" CA",
+   "id": "5325738"
+ },
+ {
+   "city":"Tampa",
+   "state":" FL",
+   "id": "4174757"
+ },
+ {
+   "city":"Honolulu",
+   "state":" HI",
+   "id": "5856195"
+ },
+ {
+   "city":"Anaheim",
+   "state":" CA",
+   "id": "5323810"
+ },
+ {
+   "city":"Aurora",
+   "state":" CO",
+   "id": "5412347"
+ },
+ {
+   "city":"Santa Ana",
+   "state":" CA",
+   "id": "5392900"
+ },
+ {
+   "city":"St. Louis",
+   "state":" MO",
+   "id": "4407084"
+ },
+ {
+   "city":"Riverside",
+   "state":" CA",
+   "id": "5387877"
+ },
+ {
+   "city":"Corpus Christi",
+   "state":" TX",
+   "id": "4683416"
+ },
+ {
+   "city":"Pittsburgh",
+   "state":" PA",
+   "id": "5206379"
+ },
+ {
+   "city":"Lexington",
+   "state":" KY",
+   "id": "4297999"
+ },
+ {
+   "city":"Anchorage",
+   "state":" AK",
+   "id": "5879400"
+ },
+ {
+   "city":"Stockton",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Cincinnati",
+   "state":" OH",
+   "id": ""
+ },
+ {
+   "city":"St. Paul",
+   "state":" MN",
+   "id": ""
+ },
+ {
+   "city":"Toledo",
+   "state":" OH",
+   "id": ""
+ },
+ {
+   "city":"Newark",
+   "state":" NJ",
+   "id": "",
+ },
+ {
+   "city":"Greensboro",
+   "state":" NC",
+   "id": ""
+ },
+ {
+   "city":"Plano",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Henderson",
+   "state":" NV",
+   "id": ""
+ },
+ {
+   "city":"Lincoln",
+   "state":" NE",
+   "id": ""
+ },
+ {
+   "city":"Buffalo",
+   "state":" NY",
+   "id": ""
+ },
+ {
+   "city":"Fort Wayne",
+   "state":" IN",
+   "id": ""
+ },
+ {
+   "city":"Jersey ",
+   "state":" NJ",
+   "id": ""
+ },
+ {
+   "city":"Chula Vista",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Orlando",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"St. Petersburg",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Norfolk",
+   "state":" VA",
+   "id": ""
+ },
+ {
+   "city":"Chandler",
+   "state":" AZ",
+   "id": ""
+ },
+ {
+   "city":"Laredo",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Madison",
+   "state":" WI",
+   "id": ""
+ },
+ {
+   "city":"Durham",
+   "state":" NC",
+   "id": ""
+ },
+ {
+   "city":"Lubbock",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Winston-Salem",
+   "state":" NC",
+   "id": ""
+ },
+ {
+   "city":"Garland",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Glendale",
+   "state":" AZ",
+   "id": ""
+ },
+ {
+   "city":"Hialeah",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Reno",
+   "state":" NV",
+   "id": ""
+ },
+ {
+   "city":"Baton Rouge",
+   "state":" LA",
+   "id": ""
+ },
+ {
+   "city":"Irvine",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Chesapeake",
+   "state":" VA",
+   "id": ""
+ },
+ {
+   "city":"Irving",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Scottsdale",
+   "state":" AZ",
+   "id": ""
+ },
+ {
+   "city":"North Las Vegas",
+   "state":" NV",
+   "id": ""
+ },
+ {
+   "city":"Fremont",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Gilbert",
+   "state":" AZ",
+   "id": ""
+ },
+ {
+   "city":"San Bernardino",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Boise",
+   "state":" ID",
+   "id": ""
+ },
+ {
+   "city":"Birmingham",
+   "state":" AL",
+   "id": ""
+ },
+ {
+   "city":"Rochester",
+   "state":" NY",
+   "id": ""
+ },
+ {
+   "city":"Richmond",
+   "state":" VA",
+   "id": ""
+ },
+ {
+   "city":"Spokane",
+   "state":" WA",
+   "id": ""
+ },
+ {
+   "city":"Des Moines",
+   "state":" IA",
+   "id": ""
+ },
+ {
+   "city":"Montgomery",
+   "state":" AL",
+   "id": ""
+ },
+ {
+   "city":"Modesto",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Fayetteville",
+   "state":" NC",
+   "id": ""
+ },
+ {
+   "city":"Tacoma",
+   "state":" WA",
+   "id": ""
+ },
+ {
+   "city":"Shreveport",
+   "state":" LA",
+   "id": ""
+ },
+ {
+   "city":"Fontana",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Oxnard",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Aurora",
+   "state":" IL",
+   "id": ""
+ },
+ {
+   "city":"Moreno Valley",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Akron",
+   "state":" OH",
+   "id": ""
+ },
+ {
+   "city":"Yonkers",
+   "state":" NY",
+   "id": ""
+ },
+ {
+   "city":"Columbus",
+   "state":" GA",
+   "id": ""
+ },
+ {
+   "city":"Augusta-Richmond",
+   "state":" GA",
+   "id": ""
+ },
+ {
+   "city":"Little Rock",
+   "state":" ArKS",
+   "id": ""
+ },
+ {
+   "city":"Amarillo",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Mobile",
+   "state":" AL",
+   "id": ""
+ },
+ {
+   "city":"Huntington Beach",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Glendale",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Grand Rapids",
+   "state":" MI",
+   "id": ""
+ },
+ {
+   "city":"Salt Lake",
+   "state":" UT",
+   "id": ""
+ },
+ {
+   "city":"Tallahassee",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Huntsville",
+   "state":" AL",
+   "id": ""
+ },
+ {
+   "city":"Worcester",
+   "state":" MA",
+   "id": ""
+ },
+ {
+   "city":"Knoxville",
+   "state":" TN",
+   "id": ""
+ },
+ {
+   "city":"Grand Prairie",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Newport News",
+   "state":" VA",
+   "id": ""
+ },
+ {
+   "city":"Brownsville",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Santa Clarita",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Overland Park",
+   "state":" KS",
+   "id": ""
+ },
+ {
+   "city":"Providence",
+   "state":" RI",
+   "id": ""
+ },
+ {
+   "city":"Jackson",
+   "state":" MS",
+   "id": ""
+ },
+ {
+   "city":"Garden Grove",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Oceanside",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Chattanooga",
+   "state":" TN",
+   "id": ""
+ },
+ {
+   "city":"Fort Lauderdale",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Rancho Cucamonga",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Santa Rosa",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Port St. Lucie",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Ontario",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Tempe",
+   "state":" AZ",
+   "id": ""
+ },
+ {
+   "city":"Vancouver",
+   "state":" WA",
+   "id": ""
+ },
+ {
+   "city":"Springfield",
+   "state":" MO",
+   "id": ""
+ },
+ {
+   "city":"Cape Coral",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Pembroke Pines",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Sioux Falls",
+   "state":" SD",
+   "id": ""
+ },
+ {
+   "city":"Peoria",
+   "state":" AZ",
+   "id": ""
+ },
+ {
+   "city":"Lancaster",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Elk Grove",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Corona",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Eugene",
+   "state":" OR",
+   "id": ""
+ },
+ {
+   "city":"Salem",
+   "state":" OR",
+   "id": ""
+ },
+ {
+   "city":"Palmdale",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Salinas",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Springfield",
+   "state":" MA",
+   "id": ""
+ },
+ {
+   "city":"Pasadena",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Rockford",
+   "state":" IL",
+   "id": ""
+ },
+ {
+   "city":"Pomona",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Hayward",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Fort Collins",
+   "state":" CO",
+   "id": ""
+ },
+ {
+   "city":"Joliet",
+   "state":" IL",
+   "id": ""
+ },
+ {
+   "city":"Escondido",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Kansas",
+   "state":" KS",
+   "id": ""
+ },
+ {
+   "city":"Torrance",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Bridgeport",
+   "state":" CT",
+   "id": ""
+ },
+ {
+   "city":"Alexandria",
+   "state":" VA",
+   "id": ""
+ },
+ {
+   "city":"Sunnyvale",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Cary",
+   "state":" NC",
+   "id": ""
+ },
+ {
+   "city":"Lakewood",
+   "state":" CO",
+   "id": ""
+ },
+ {
+   "city":"Hollywood",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Paterson",
+   "state":" NJ",
+   "id": ""
+ },
+ {
+   "city":"Syracuse",
+   "state":" NY",
+   "id": ""
+ },
+ {
+   "city":"Naperville",
+   "state":" IL",
+   "id": ""
+ },
+ {
+   "city":"McKinney",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Mesquite",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Clarksville",
+   "state":" TN",
+   "id": ""
+ },
+ {
+   "city":"Savannah",
+   "state":" GA",
+   "id": ""
+ },
+ {
+   "city":"Dayton",
+   "state":" OH",
+   "id": ""
+ },
+ {
+   "city":"Orange",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Fullerton",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Pasadena",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Hampton",
+   "state":" VA",
+   "id": ""
+ },
+ {
+   "city":"McAllen",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Killeen",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Warren",
+   "state":" MI",
+   "id": ""
+ },
+ {
+   "city":"West Valley",
+   "state":" UT",
+   "id": ""
+ },
+ {
+   "city":"Columbia",
+   "state":" SC",
+   "id": ""
+ },
+ {
+   "city":"New Haven",
+   "state":" CT",
+   "id": ""
+ },
+ {
+   "city":"Sterling Heights",
+   "state":" MI",
+   "id": ""
+ },
+ {
+   "city":"Olathe",
+   "state":" KS",
+   "id": ""
+ },
+ {
+   "city":"Miramar",
+   "state":" FL",
+   "id": ""
+ },
+ {
+   "city":"Thousand Oaks",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Frisco",
+   "state":" TX",
+   "id": ""
+ },
+ {
+   "city":"Cedar Rapids",
+   "state":" IA",
+   "id": ""
+ },
+ {
+   "city":"Topeka",
+   "state":" KS",
+   "id": ""
+ },
+ {
+   "city":"Visalia",
+   "state":" CA",
+   "id": ""
+ },
+ {
+   "city":"Waco",
+   "state":" TX",
+   "id": ""
+ }
+];
