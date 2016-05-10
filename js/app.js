1daf83a2accf6d21311a046d441c9467204f061b
Anthony Mejia <anthony.mejia1194@gmail.com>
2016-05-10 07:30:35 -0400

    Updated main app

--- js/app.js
+++ js/app.js
@@ -1,58 +1,139 @@
-(function () {
-  'use strict';
-  angular
-      .module('autocompleteCustomTemplateDemo', ['ngMaterial'])
-      .controller('DemoCtrl', DemoCtrl);
-  function DemoCtrl ($timeout, $q, $log) {
-    var self = this;
-    self.simulateQuery = false;
-    self.isDisabled    = false;
-    self.repos         = loadAll();
-    self.querySearch   = querySearch;
-    self.selectedItemChange = selectedItemChange;
-    self.searchTextChange   = searchTextChange;
-    // ******************************
-    // Internal methods
-    // ******************************
-    /**
-     * Search for repos... use $timeout to simulate
-     * remote dataservice call.
-     */
-    function querySearch (query) {
-      var results = query ? self.repos.filter( createFilterFor(query) ) : self.repos,
-          deferred;
-      if (self.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
-        return results;
-      }
+(function() {
+    'use strict';
+    angular
+        .module('lumn', ['ngMaterial'])
+        .controller('search', DemoCtrl);
+
+    function DemoCtrl($timeout, $q, $log, $mdToast, $scope) {
+        var self = this;
+        self.simulateQuery = true;
+        self.isDisabled = false;
+        self.allCities = loadAll();
+        self.querySearch = querySearch;
+        self.selectedItemChange = selectedItemChange;
+        self.searchTextChange = searchTextChange;
+        self.coords = coords;
+        // ******************************
+        // Internal methods
+        // ******************************
+        /**
+         * Search for allCities... use $timeout to simulate
+         * remote dataservice call.
+         */
+        function querySearch(query) {
+            var results = query ? self.allCities.filter(createFilterFor(query)) : self.allCities,
+                deferred;
+            if (self.simulateQuery) {
+                deferred = $q.defer();
+                $timeout(function() {
+                    deferred.resolve(results);
+                }, Math.random() * 1000, false);
+                return deferred.promise;
+            } else {
+                return results;
+            }
+        }
+
+        function searchTextChange(text) {
+            $log.info('Text changed to ' + text);
+        }
+
+        function selectedItemChange(item) {
+            if (item.id) {
+                alert("id: " + item.id);
+                getWeather(item, "cityID");
+            } else {
+                $mdToast.show(
+                    $mdToast.simple()
+                    .textContent('Oops! ' + item.city + ', ' + item.state + ' is currently unavailable')
+                    .hideDelay(4000)
+                    .theme("error")
+                );
+            }
+        }
+        /**
+         * Build `components` list of key/value pairs
+         */
+        function loadAll() {
+            var allCities = cities;
+            return allCities.map(function(cityItem) {
+                cityItem.value = cityItem.city.toLowerCase();
+                return cityItem;
+            });
+        }
+        /**
+         * Get Weather
+         */         
+         function getWeather(item, method) {
+            var url = "";
+            // Determine the method of getting weather results ?
+            // Set the URL to the appropriate one
+            // Call the api with the URL and handle the callback
+            // -- Animate away all the unnecessary stuff
+            // -- Use ng-include to add our template and animate data in
+            if (method === "cityID") {
+                url = "http://api.openweathermap.org/data/2.5/weather?id=" + item.id;
+                alert(url);
+                confirm(item.city + ", " + item.state + " - " + method);
+            }
+           else if (method === "geo") {               
+                url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139";
+            }
+            else {
+                alert("sorry");
+            }
+        }
+        
+        function coords() {
+            if (navigator.geolocation) { 
+                navigator.geolocation.getCurrentPosition(showPosition, showError);            
+             } 
+             else { 
+                 $mdToast.show(
+                     $mdToast.simple()
+                     .textContent("Your browser doesn't support this feature")
+                     .hideDelay(4000)
+                     .theme("error")
+                );
+             } 
+             
+             function showPosition(position) {
+    alert("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+}
+
+function showError(error) {
+	var msg;
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            msg = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            msg = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            msg = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            msg = "An unknown error occurred."
+            break;
     }
-    function searchTextChange(text) {
-      $log.info('Text changed to ' + text);
+    $mdToast.show(
+        $mdToast.simple()
+             .textContent(msg)
+             .hideDelay(4000)
+             .theme("error")
+      );
+}              
+
+         }
+        /**
+         * Create filter function for a query string
+         */
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(item) {
+                return (item.value.indexOf(lowercaseQuery) === 0);
+            };
+        }
     }
-    function selectedItemChange(item) {
-      $log.info('Item changed to ' + JSON.stringify(item));
-    }
-    /**
-     * Build `components` list of key/value pairs
-     */
-    function loadAll() {
-      var repos = cities;
-      return repos.map( function (repo) {
-        repo.value = repo.city.toLowerCase();
-        return repo;
-      });
-    }
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(item) {
-        return (item.value.indexOf(lowercaseQuery) === 0);
-      };
-    }
-  }
 })();
